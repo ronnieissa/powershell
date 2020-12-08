378fa50d54a7dd42e879944298096fe246ce1a56
ronnieissa <3667610+ronnieissa@users.noreply.github.com>
2020-12-08 10:24:33 -0800

    Update inventory_v3.ps1

--- inventory_v3.ps1
+++ inventory_v3.ps1
@@ -6,23 +6,23 @@ Import-Module Microsoft.PowerShell.Utility -force -Verbose
 Import-Module ActiveDirectory -Force -Verbose
 Import-Module FailoverClusters -Force -Verbose -Prefix w
 
-Get-ChildItem -Path "$home\documents\windowspowershell\modules" -Recurse -Include *.psd1| unblock-file
-Import-Module "vmware.vimautomation.core" -prefix 'v' -Force -verbose 
-connect-vviserver vcenter.domain.local -force -verbose
+#Get-ChildItem -Path "$home\documents\windowspowershell\modules" -Recurse -Include *.psd1| unblock-file
+#Import-Module "vmware.vimautomation.core" -prefix 'v' -Force -verbose 
+#connect-vviserver vcenter01.domain.local -force -verbose
 
 
 ## Create the Directory to store the inventory files
 $inv_dir = "$home\desktop\VM_INV\"
 if ((test-path $inv_dir)) {rm "$home\Desktop\VM_INV\FAILED*" -force}
 if (!(test-path $inv_dir)) {mkdir -path "$home\Desktop\" -name "VM_INV" -force}
-#$environment = ""
+$environment = 'server-name*'
 
 #############################################################
 ## Create Array of computer names you want to inventory
-$computers = @(Get-ADComputer -Filter {name -like 'aws-server*'} | select-object -ExpandProperty name | select-string -pattern "$environment-\D\D\D\d\d")
+$computers = @(Get-ADComputer -Filter {name -like $environment} | select-object -ExpandProperty name | select-string -pattern ".\D\D\D\d\d")
 
 ## Get 1 specific computers
-#$computers = @(get-adcomputer -server "computername" -filter {name -like 'F03-SMAXP-SQL01'} | select-object -ExpandProperty name)
+#$computers = @(get-adcomputer -server "" -filter {name -like ''} | select-object -ExpandProperty name)
 
 ## Get list of computers from file
 #$computers = @(Get-Content -Path "$home\desktop\server.txt" )
@@ -61,6 +61,11 @@ $Computers | ForEach-Object {
                 $WSFCinstalled = Get-CimInstance -CimSession $cimsession -ClassName win32_optionalfeature -Filter "name like 'failovercluster-fullserver'"
                 $test_net = Invoke-Command -Session $pssession -ScriptBlock {Test-NetConnection -computername domain.local}
                 $nslookup = Invoke-Command -Session $pssession -scriptblock {nslookup $env:computername}
+                $timezone = Invoke-Command -Session $pssession -ScriptBlock {Get-TimeZone}
+                $date = Invoke-Command -Session $pssession -ScriptBlock {Get-date}
+                $ntp = Invoke-Command -Session $pssession -ScriptBlock {w32tm.exe /query /status}
+                $group_admin = Invoke-Command -Session $pssession -ScriptBlock {get-localgroupmember "administrators" | Select-Object -property * -ExcludeProperty Pscomputername,psshowcomputername,sid,principalsource,objectclass,runspaceid}
+                $group_rdp = Invoke-Command -Session $pssession -ScriptBlock {get-localgroupmember "Remote Desktop users" | Select-Object -property * -ExcludeProperty Pscomputername,psshowcomputername,sid,principalsource,objectclass,runspaceid}
                 #$vminfo = get-vvm -name $COMPUTERNAME -debug
                 
                 #if ($WSFCinstalled) {Start-Job -Session $pssession -ArgumentList $computername -ScriptBlock {(Get-wCluster -name "$computername.domain.local" | Select-Object -ExcludeProperty sharedvolumesecuritydescriptor,Pscomputername,psshowcomputername)} -OutVariable WSFC}
@@ -84,33 +89,34 @@ $Computers | ForEach-Object {
                                     #'Memory = ' + $vminfo.MemoryMB
                                     #'VM_Host = ' + $vminfo.VMHost.name
                                 #)
-                                Processors = $cs.NumberOfLogicalProcessors
-                                Model = $cs.Model
-                                Mfgr = $cs.Manufacturer
-                                DNSname = $cs.DNSHostName
-                                Name = $cs.Name
+                                HW_Processors = $cs.NumberOfLogicalProcessors
+                                HW_Model = $cs.Model
+                                HW_Mfgr = $cs.Manufacturer
+                                A_DNSname = $cs.DNSHostName
+                                A_Name = $cs.Name
                                 OS_InstallDate = $os.InstallDate.ToShortDateString()
-                                Domain = $cs.Domain
+                                A_Domain = $cs.Domain
                                 OS_Status = $cs.Status
                                 Apps = @(
                                     foreach ($app in $apps) {$app.vendor + '_______________'+ $app.name + '_______________' + $app.version}
                                 ) | Sort-Object
                                 OS_buildnum = $os.BuildNumber
-                                Lastbootup = $os.LastBootUpTime.ToShortDateString()
+                                A_Lastbootup = ($os.LastBootUpTime.ToShortDateString()) + "  " + ($os.LastBootUpTime.ToShorttimeString())
                                 #LocalTime = $os.LocalDateTime.ToShortDateString()
                                 #Serial = $os.SerialNumber
                                 OS_version = $os.Version
                                 #ServiceTag = $chassis.serialnumber
-                                Inventory_Time = @{
-                                    Time = $datetimenow.ToShortTimeString()
-                                    Date = $datetimenow.ToShortDateString()
-                                }
+                                A_Inventory_Time = ($datetimenow.ToShortDateString()) + "  " + ($datetimenow.ToShortTimeString())
+                                Timezone = $timezone | Select-Object -Property ID,DisplayName,standardname,daylightname                              
+                                
+                                #Time_sync = @($ntp | % {})
+
                                 Network = @(
                                     foreach ($adapter in $netinfo) {if ($adapter.MACAddress -ne $null) {$adapter | Select-Object -Property caption,Description,Macaddress,IPAddress,IPSubnet,DefaultIPGateway,DHCPEnabled,DHCPServer,DNSServerSearchOrder,servicename}}
                                 )
 
                                 <###>
-                                Services = @(
+                                Services_Running = @(
                                     foreach ($service in $services) {$service.DisplayName +'_______________'+ $service.name  + '_______________' +  $service.startname | Sort-Object -Property $service.Name}
                                 ) | Sort-Object
 
@@ -126,6 +132,9 @@ $Computers | ForEach-Object {
                                 WSFC = if ($WSFCinstalled.InstallState -eq 1){'TRUE : Failover Clustering is installed'} else {
                                     'FALSE : Failover Clustering is NOT installed'
                                 }
+                                
+                                Group_Local_Administrators = $group_admin | % { $_.name }
+                                Group_Remote_Desktop = $group_rdp | % {$_.name}
                             }
             }
 
